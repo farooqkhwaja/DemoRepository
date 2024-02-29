ec9043b9298438872c231693bdcbcbe1666ddd53
farooqkhwaja <farooqkhoja16@gmail.com>
2024-02-29 12:25:18 +0100

    Made an input to ask for name

---
+++ FirstRepoOnGithHub/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstRepoOnGithHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give in your name: ");
+            string name = Console.ReadLine();
+
+
+        }
+    }
+}
