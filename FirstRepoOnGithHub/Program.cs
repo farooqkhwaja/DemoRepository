9349eedff84cd4283826a5fcbb066443d4e561bb
farooqkhwaja <farooqkhoja16@gmail.com>
2024-02-29 14:16:05 +0100

    An input to give in your name

--- FirstRepoOnGithHub/Program.cs
+++ FirstRepoOnGithHub/Program.cs
@@ -6,7 +6,10 @@
         {
             Console.Write("Give in your name: ");
             var name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.WriteLine("Give in your age: ");
             var number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number);
         }
     }
 }
