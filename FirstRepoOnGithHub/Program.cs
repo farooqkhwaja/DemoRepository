38e495e8919f3b77e34c06fde40c7fd9c3b5eed5
Randy Marsh <dura.software@outlook.com>
2024-02-29 13:54:30 +0100

    Test commit to feature

--- FirstRepoOnGithHub/Program.cs
+++ FirstRepoOnGithHub/Program.cs
@@ -5,9 +5,8 @@
         static void Main(string[] args)
         {
             Console.Write("Give in your name: ");
-            string name = Console.ReadLine();
-
-
+            var name = Console.ReadLine();
+            
         }
     }
 }
